c376419901a2988e8a05352b1123363fee6b6c7c
Pedro Julio <soaresjuliopedro@gmail.com>
2020-08-12 15:25:20 -0300

    Implementado slides na página portfolio

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+if (window.SimpleSlide) {
+
+    new SimpleSlide({
+        slide: "quote",
+        auto: true,
+        time: 5000
+    })
+    new SimpleSlide({
+        slide: "portfolio",
+        auto: true,
+        nav: true,
+        time: 5000
+    })
+}
